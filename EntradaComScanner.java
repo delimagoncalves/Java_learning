6a3893314b469a09be6919b26ef92b6ce2d91eb1
Gilson <>
2022-02-22 09:31:03 -0300

    Entrada de dados com Scanner

---
+++ EntradaComScanner.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class EntradaComScanner {
+
+	public static void main(String[] args) {
+	
+		float largura, comprimento, area, perimetro;
+		Scanner dado;
+		
+		try {
+			
+			System.out.println("Entre com o comprimento: ");
+			dado = new Scanner(System.in);
+			comprimento = dado.nextFloat();
+			
+			System.out.println("Entre com a largura: ");
+			dado = new Scanner(System.in);
+			largura = dado.nextFloat();
+			
+			area = comprimento * largura;
+			perimetro = (comprimento*2)+(largura*2);
+			
+			System.out.println("Àrea do retângulo: "+area);
+			System.out.println("Peŕimetro do retângulo: "+perimetro);
+			
+		}catch(Exception ex)
+		{
+			System.out.println("Houve um erro: "+ex.toString());
+		}
+
+	}
+
+}
